c833d132bf32417743038d8a98aec8f23ee1647b
eldrianayera <eldrianayera710@gmail.com>
2024-09-03 13:39:35 +0800

    connecting html with js

--- index.js
+++ index.js
@@ -1,26 +1,15 @@
-// It must start with a hashtag #.
-// Ignore spaces in the input.
-// All words must have their first letter capitalized.
-// If the final result is longer than 140 chars it must return false.
-// If the input or the result is an empty string it must return false.
-// const capitalizer = (string) => {
-//   const words = string.split(" ");
-//   console.log(words);
+const input = document.getElementById("text");
+const output = document.getElementById("output");
+const button = document.getElementById("button");
 
-//   return;
-// };
-
-const hashtagGenerator = (string) => {
+const hashtagGenerator = () => {
+  const string = input.value;
   if (typeof string !== "string") {
     return "Input a string";
   }
 
   const separated = string.split(" ");
-  // let array = [];
-  // for (const word of separated) {
-  //   const capWord = word.charAt(0).toUpperCase() + word.slice(1);
-  //   array.push(capWord);
-  // }
+
   const capitalizedWords = separated.map(
     (word) => word.charAt(0).toUpperCase() + word.slice(1)
   );
@@ -29,17 +18,19 @@ const hashtagGenerator = (string) => {
   const regex = /\s+/g;
   const clearInput = joined.replace(regex, "");
   if (clearInput.length === 0) {
-    return false;
+    output.innerText = "false input";
   }
 
   const hashtag = "#" + clearInput;
 
   if (hashtag.length > 140) {
-    return false;
+    output.innerText = "false input";
   } else {
-    return hashtag;
+    output.innerText = hashtag;
   }
 };
+
+button.addEventListener("click", hashtagGenerator);
 
 console.log(hashtagGenerator("JavaScript")); // #JavaScript
 console.log(hashtagGenerator("Do we have a Hashtag")); // #DoWeHaveAHashtag
